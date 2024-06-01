42d559f90707fd989416ca2fd02a5bb7afa3b891
Thuy Diem Ha Pham <dha.pham@gmail.com>
2024-05-31 20:23:35 -0400

    set up SideBar, show hide Side Bar

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,13 +1,17 @@
-import SideBar from "./SideBar"
-
+import SideBar from "./SideBar";
+import './Admin.scss';
+import { FaHeart, FaBars } from 'react-icons/fa';
+import { useState } from "react";
 const Admin = (porps) => {
+    const [collapsed, setCollapsed] = useState(false);
     return (
         <div className="admin-container">
             <div className="admin-sidebar">
-                <SideBar />
+                <SideBar collapsed={collapsed}/>
             </div>
             <div className="admin-cotent">
-
+                <FaBars onClick={() =>setCollapsed(!collapsed)}/>
+                content goes here
             </div>
 
         </div>
